9a37e43b9c94b44c5cb2ad308ffc5aef9bc0a535
juntaozhou05 <350529307@qq.com>
2018-01-08 13:32:36 +0800

    add test

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from './Button/Button'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>主页</div>
+        <Button></Button>
+      </div>
+    );
+  }
+}
+
+export default App;
