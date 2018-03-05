f65faa73d8f32d4384554275c5ee1563f3d5f0fa
juntaozhou05 <350529307@qq.com>
2018-03-05 15:08:42 +0800

    update

--- src/components/Detailplay/index.js
+++ src/components/Detailplay/index.js
@@ -4,17 +4,31 @@ import "./detailplay.css";
 class Detailplay extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      bgimg:
+        "http://oiq8j9er1.bkt.clouddn.com/music_%E6%88%91%E8%BF%98%E6%83%B3%E5%A5%B9.jpg"
+    };
   }
   render() {
     return (
       <div className="detailplay">
-        <div className="top">
+        <div
+          className="detailCover"
+          style={{ backgroundImage: "url(" + this.state.bgimg + ")" }}
+        />
+        <div className="detailTop">
           <div className="left">
             <i class="back icon-back" />
             <div className="title">
-              <div>ff</div>
+              <div className="singName">小酒窝</div>
+              <div className="singer">林俊杰，蔡卓妍</div>
             </div>
+          </div>
+          <i class="share icon-share" />
+        </div>
+        <div className="detailContent">
+          <div className="singImg">
+            <img src={this.state.bgimg} alt="" />
           </div>
         </div>
       </div>
